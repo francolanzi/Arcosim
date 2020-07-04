baa7d5f520e30ce139136086860dfbf32789bb5a
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-07-04 20:30:30 -0300

    cpnt input and output interface added

--- js/cpnt/cpnt.js
+++ js/cpnt/cpnt.js
@@ -4,6 +4,29 @@ class Component
     {
         if (this.constructor == Component)
             throw new Error("Component class can't be instantiated");
+        
+        this.inputs = new Map();
+        this.outputs = new Map();
+    }
+
+    getInput(id)
+    {
+        return this.inputs.get(id);
+    }
+
+    getOutput(id)
+    {
+        return this.outputs.get(id);
+    }
+
+    inputList()
+    {
+        return this.inputs.keys();
+    }
+
+    outputList()
+    {
+        return this.outputs.keys();
     }
 }
 
