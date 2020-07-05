1695e81d5b610b2d30415aac1f90a034f6d4b440
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-07-05 00:54:31 -0300

    cpnt classes added

--- js/cpnt/cpnt.js
+++ js/cpnt/cpnt.js
@@ -30,7 +30,40 @@ class Component
     }
 }
 
+class ALU extends Component { }
+class Clock extends Component { }
+class ControlStore extends Component { }
+class Decoder extends Component { }
+class Increment extends Component { }
+class Latch extends Component { }
+class MAR extends Component { }
+class MBR extends Component { }
+class MIR extends Component { }
+class MPC extends Component { }
+class MUX extends Component { }
+class MicroSequenceLogic extends Component { }
+class Registers extends Component { }
+class Shifter extends Component { }
+
+const cpnt = new Map();
+
+cpnt.set('ALU', ALU);
+cpnt.set('Clock', Clock);
+cpnt.set('ControlStore', ControlStore);
+cpnt.set('Decoder', Decoder);
+cpnt.set('Increment', Increment);
+cpnt.set('Latch', Latch);
+cpnt.set('MAR', MAR);
+cpnt.set('MBR', MBR);
+cpnt.set('MIR', MIR);
+cpnt.set('MPC', MPC);
+cpnt.set('MUX', MUX);
+cpnt.set('MicroSequenceLogic', MicroSequenceLogic);
+cpnt.set('Registers', Registers);
+cpnt.set('Shifter', Shifter);
+
 module.exports =
 {
-    Component: Component
+    list: () => cpnt.keys(),
+    new: key => new (cpnt.get(key))()
 };
