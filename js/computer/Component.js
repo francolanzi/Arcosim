4f6dcd1f4e113039f0a14e08583436868395ca54
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-07-05 21:16:28 -0300

    Component id management added

--- js/computer/Component.js
+++ js/computer/Component.js
@@ -1,5 +1,9 @@
+const { _count } = require("./classes/ALU");
+
 class Component
 {
+    static _count = new Map();
+
     static get image()
     {
         throw new Error('image static property must be overrided');
@@ -10,8 +14,17 @@ class Component
         if (this.constructor == Component)
             throw new Error('Component class can not be instantiated');
         
+        var count = Component._count.get(this.constructor.name);
+        this._id = count ? ++count : 1;
+        Component._count.set(this.constructor.name, this._id);
+        
         this.inputs = new Map();
         this.outputs = new Map();
+    }
+
+    get id()
+    {
+        return this._id;
     }
 
     getInput(id)
