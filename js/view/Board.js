e5e395f37798f1d4a28723b110e989d315b6b5b5
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-07-19 14:12:18 -0300

    Board transformed into StyledElement

--- js/view/Board.js
+++ js/view/Board.js
@@ -1,4 +1,6 @@
-class Board extends HTMLElement
+const StyledElement = require('./StyledElement');
+
+class Board extends StyledElement
 {
     constructor()
     {
@@ -6,17 +8,7 @@ class Board extends HTMLElement
 
         this._cpnts = new Map();
 
-        this.style.display = 'block';
-        this.style.backgroundColor = '#f8f9fa';
-        this.style.margin = 0;
-        this.style.padding = 0;
-        this.style.width = '100%'
-        this.style.height = '100%'
-        this.style.boxSizing = 'border-box';
-        this.style.position = 'absolute';
-        this.style.top = 0;
-        this.style.left = 0;
-        this.style.zIndex = 0;
+        this.addStyles('css/Board.css');
     }
 
     addCpnt(instance)
@@ -25,13 +17,13 @@ class Board extends HTMLElement
         if (!this._cpnts.has(key))
         {
             this._cpnts.set(key, instance);
-            this.appendChild(instance);
+            this.shadow.appendChild(instance);
 
             instance.addEventListener('drag', () =>
                 document.body.appendChild(instance));
             
             instance.addEventListener('drop', () =>
-                this.appendChild(instance));
+                this.shadow.appendChild(instance));
         }
         return this.getCpnt(key);
     }
