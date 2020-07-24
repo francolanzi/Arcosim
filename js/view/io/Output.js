680bbc1c5186cdb84d3182eccce02daf43b590d1
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-07-24 19:25:53 -0300

    Output prevents non-click focus

--- js/view/io/Output.js
+++ js/view/io/Output.js
@@ -1,6 +1,21 @@
 const IOElement = require('./IO');
 
-class OutputElement extends IOElement {}
+class OutputElement extends IOElement {
+  constructor(cpnt, name, x, y) {
+    super(cpnt, name, x, y);
+
+    let clicked = false;
+
+    this.addEventListener('mousedown', () => clicked = true);
+    this.addEventListener('mouseup', () => clicked = false);
+
+    this.addEventListener('focus', () => {
+      if (!clicked) {
+        this.blur();
+      }
+    });
+  }
+}
 
 customElements.define('cpnt-output', OutputElement);
 
