c0475167e993e5cc4972a1a8a5ed580ac2c0487a
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-07-24 19:17:12 -0300

    First computer links version created

--- js/computer/io/Input.js
+++ js/computer/io/Input.js
@@ -1,5 +1,42 @@
 const IO = require('./IO');
 
-class Input extends IO {}
+class Input extends IO {
+  get value() {
+    return this._output ? this._output.value : null;
+  }
+
+  get linked() {
+    return this._output !== null;
+  }
+
+  constructor(name) {
+    super(name);
+    this._output = null;
+
+    this._receive = (() => this.dispatchEvent(new Event('receive'))).bind(this);
+    this._stop = (() => this.dispatchEvent(new Event('stop'))).bind(this);
+  }
+
+  link(output) {
+    if (!this.linked) {
+      this._output = output;
+
+      output.addEventListener('send', this._receive);
+      output.addEventListener('stop', this._stop);
+
+      if (output.value !== null) {
+        this._receive();
+      }
+    }
+  }
+
+  unlink() {
+    if (this.linked) {
+      this._output.removeEventListener('send', this._receive);
+      this._output.removeEventListener('stop', this._stop);
+      this._output = null;
+    }
+  }
+}
 
 module.exports = Input;
--- js/computer/io/Output.js
+++ js/computer/io/Output.js
@@ -1,5 +1,28 @@
 const IO = require('./IO');
 
-class Output extends IO {}
+class Output extends IO {
+  get value() {
+    return this._value;
+  }
+
+  constructor(name) {
+    super(name);
+    this._value = null;
+  }
+
+  send(value) {
+    if (!this._sending) {
+      this._value = value;
+      this.dispatchEvent(new Event('send'));
+    }
+  }
+
+  stop() {
+    if (this._sending) {
+      this._value = null;
+      this.dispatchEvent(new Event('stop'));
+    }
+  }
+}
 
 module.exports = Output;
