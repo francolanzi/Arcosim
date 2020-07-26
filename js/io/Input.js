5cd12c2cfc2d2b7a1b4cddb170fe510f676e41e4
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-07-26 18:23:47 -0300

    Input link and unlink methods added

--- js/io/Input.js
+++ js/io/Input.js
@@ -20,26 +20,40 @@ class Input extends IO {
 
     this._value = null;
 
-    let linked = false;
-    let clicked = false;
+    this._linked = false;
+    this._clicked = false;
 
-    this.addEventListener('mousedown', () => clicked = true);
-    this.addEventListener('mouseup', () => clicked = false);
+    this.addEventListener('mousedown', () => this._clicked = true);
+    this.addEventListener('mouseup', () => this._clicked = false);
 
     this.addEventListener('focus', ev => {
-      const output = ev.relatedTarget;
       this.blur();
 
-      if (clicked) {
-        if (linked) {
-          linked = false;
-          this.dispatchEvent(new Event('unlink'));
-        } else if (output && output.constructor == Output) {
-          linked = true;
-          this.dispatchEvent(new CustomEvent('link', { detail: output }));
+      if (this._clicked) {
+        if (this._linked) {
+          this.unlink();
+        } else {
+          const output = ev.relatedTarget;
+          if (output && output.constructor == Output) {
+            this.link(output);
+          }
         }
       }
     });
+  }
+
+  link(output) {
+    if (!this._linked) {
+      this._linked = true;
+      this.dispatchEvent(new CustomEvent('link', { detail: output }));
+    }
+  }
+
+  unlink() {
+    if (this._linked) {
+      this._linked = false;
+      this.dispatchEvent(new Event('unlink'));
+    }
   }
 
   receive(value) {
