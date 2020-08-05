24376de27b591abb13f03ef6518b655bbd8bb05c
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-08-05 17:48:26 -0300

    Registers run method added

--- js/cpnts/Registers.js
+++ js/cpnts/Registers.js
@@ -19,14 +19,43 @@ class Registers extends Component {
 
     this._registers = new Array(16).fill(0);
 
-    this.addInput('DecoderA', 20.75, 0);
-    this.addInput('DecoderB', 46.5, 0);
-    this.addInput('DecoderC', 72.25, 0);
-    this.addInput('Input', 0, 44.5);
-    this.addInput('Clock', 0, 19.75);
+    this._decoderA = this.addInput('Decoder A', 20.75, 0);
+    this._decoderB = this.addInput('Decoder B', 46.5, 0);
+    this._decoderC = this.addInput('Decoder C', 72.25, 0);
+    this._inputC = this.addInput('C', 0, 44.5);
+    this._clock = this.addInput('Clock', 0, 19.75);
 
-    this.addOutput('OutputA', 93, 28);
-    this.addOutput('OutputB', 93, 61);
+    this._outputA = this.addOutput('A', 93, 28);
+    this._outputB = this.addOutput('B', 93, 61);
+  }
+
+  run() {
+    function encode(decoded) {
+      let count = -1;
+      while (decoded) {
+        decoded = decoded >> 1;
+        count++;
+      }
+      return count;
+    }
+
+    const indexA = encode(this._decoderA.value);
+    const indexB = encode(this._decoderB.value);
+    const indexC = encode(this._decoderC.value);
+
+    if (indexA >= 0 && indexA < this._registers.length) {
+      this._outputA.value = this._registers[indexA];
+    }
+
+    if (indexB >= 0 && indexB < this._registers.length) {
+      this._outputB.value = this._registers[indexB];
+    }
+
+    if (this._clock.value && indexC >= 0 && indexC < this._registers.length) {
+      this._registers[indexC] = this._inputC.value;
+    }
+
+    return super.run();
   }
 
   addRegister() {
