44a56118fd43e22e74134bd7b65524f0d78de04e
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-08-05 17:43:43 -0300

    Computer run and reset methods added

--- js/Computer.js
+++ js/Computer.js
@@ -24,6 +24,35 @@ class Computer {
     console.log(`${key} removed`);
     return this._cpnts.delete(key);
   }
+
+  run() {
+    let time = 0;
+    let running = true;
+    let changed = false;
+
+    const stop = () => {
+      running = false;
+      this._cpnts.forEach(cpnt =>
+        cpnt.removeEventListener('stop', stop));
+    };
+
+    this._cpnts.forEach(cpnt =>
+      cpnt.addEventListener('stop', stop));
+
+    do {
+      console.log(`Time = ${time}`);
+      do {
+        changed = false;
+        this._cpnts.forEach(cpnt =>
+          changed = changed || cpnt.run(time));
+      } while (running && changed);
+      time++;
+    } while(running);
+  }
+
+  reset() {
+    this._cpnts.forEach(cpnt => cpnt.reset());
+  }
 }
 
 module.exports = Computer;
