7f84c99e6dbd6c01f1818e60dfd7229976d0c4c8
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-08-05 17:48:04 -0300

    Decoder run method added

--- js/cpnts/Decoder.js
+++ js/cpnts/Decoder.js
@@ -8,11 +8,22 @@ class Decoder extends Component {
   constructor(top, left) {
     super(top, left);
 
-    this.addInput('Encoded', 77, 14.5);
-    this.addInput('Enable', 38.5, 0);
-    this.addInput('Clock', 38.5, 29);
+    this._encoded = this.addInput('Encoded', 77, 14.5);
+    this._enable = this.addInput('Enable', 38.5, 0);
+    this._clock = this.addInput('Clock', 38.5, 29);
 
-    this.addOutput('Decoded', 0, 14.5);
+    this._decoded = this.addOutput('Decoded', 0, 14.5);
+  }
+
+  run() {
+    const clock = !this._clock.linked || this._clock.value;
+    const enable = !this._enable.linked || this._enable.value;
+
+    if (clock && enable) {
+      this._decoded.value = 1 << this._encoded.value;
+    }
+
+    return super.run();
   }
 }
 
