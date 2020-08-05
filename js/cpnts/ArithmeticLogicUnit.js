cf73f01b8ea9d7f71ca720fde4f3c4391aaddae8
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-08-05 17:48:42 -0300

    ALU run method added

--- js/cpnts/ArithmeticLogicUnit.js
+++ js/cpnts/ArithmeticLogicUnit.js
@@ -42,17 +42,70 @@ class ArithmeticLogicUnit extends Component {
 
     this._functions = new Map();
 
-    this.addInput('InputA', 11.5, 0);
-    this.addInput('InputB', 69, 0);
-    this.addInput('Function', 70, 29.5);
+    this._inputA = this.addInput('A', 11.5, 0);
+    this._inputB = this.addInput('B', 69, 0);
+    this._function = this.addInput('Function', 70, 29.5);
 
-    this.addOutput('Result', 39.5, 44);
-    this.addOutput('Control', 75, 14.5);
+    this._result = this.addOutput('Result', 39.5, 44);
+    this._control = this.addOutput('Control', 75, 14.5);
 
     this.setFunction(0, 0);
     this.setFunction(1, 5);
     this.setFunction(2, 8);
     this.setFunction(3, 10);
+  }
+
+  run() {
+    switch(this._functions.get(this._function.value)) {
+    case 0:
+      this._result.value = this._inputA.value + this._inputB.value;
+      break;
+    case 1:
+      this._result.value = this._inputA.value - this._inputB.value;
+      break;
+    case 2:
+      this._result.value = this._inputA.value * this._inputB.value;
+      break;
+    case 3:
+      this._result.value = this._inputA.value / this._inputB.value;
+      break;
+    case 4:
+      this._result.value = this._inputB.value / this._inputA.value;
+      break;
+    case 5:
+      this._result.value = this._inputA.value & this._inputB.value;
+      break;
+    case 6:
+      this._result.value = this._inputA.value | this._inputB.value;
+      break;
+    case 7:
+      this._result.value = this._inputA.value ^ this._inputB.value;
+      break;
+    case 8:
+      this._result.value = this._inputA.value;
+      break;
+    case 9:
+      this._result.value = this._inputB.value;
+      break;
+    case 10:
+      this._result.value = ~ this._inputA.value;
+      break;
+    case 11:
+      this._result.value = ~ this._inputB.value;
+      break;
+    default:
+      break;
+    }
+
+    if (this._result.value > 0) {
+      this._control.value = 0b00;
+    } else if (this._result.value === 0) {
+      this._control.value = 0b01;
+    } else {
+      this._control.value = 0b10;
+    }
+
+    return super.run();
   }
 
   getFunction(index) {
