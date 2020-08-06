932ab3062476162b2ab37d5a7c67fb521a37e244
Franco Lanzillotta <franco.lanzi96@gmail.com>
2020-08-06 11:14:46 -0300

    MicroSequenceLogic run method added

--- js/cpnts/MicroSequenceLogic.js
+++ js/cpnts/MicroSequenceLogic.js
@@ -37,15 +37,44 @@ class MicroSequenceLogic extends Component {
 
     this._conditions = new Map();
 
-    this.addInput('Condition', 61, 20.5);
-    this.addInput('Control', 0, 20.5);
+    this._condition = this.addInput('Condition', 61, 20.5);
+    this._control = this.addInput('Control', 0, 20.5);
 
-    this.addOutput('Jump', 30.5, 0);
+    this._jump = this.addOutput('Jump', 30.5, 0);
 
     this.setCondition(0, 0);
     this.setCondition(1, 2);
     this.setCondition(2, 3);
     this.setCondition(3, 6);
+  }
+
+  run() {
+    switch (this._conditions.get(this._condition.value)) {
+    case 0:
+      this._jump.value = 0;
+      break;
+    case 1:
+      this._jump.value = this._control.value === 0b00 ? 1 : 0;
+      break;
+    case 2:
+      this._jump.value = this._control.value === 0b10 ? 1 : 0;
+      break;
+    case 3:
+      this._jump.value = this._control.value === 0b01 ? 1 : 0;
+      break;
+    case 4:
+      this._jump.value = this._control.value !== 0b10 ? 1 : 0;
+      break;
+    case 5:
+      this._jump.value = this._control.value !== 0b00 ? 1 : 0;
+      break;
+    case 6:
+      this._jump.value = 1;
+      break;
+    default:
+      break;
+    }
+    return super.run();
   }
 
   getCondition(index) {
