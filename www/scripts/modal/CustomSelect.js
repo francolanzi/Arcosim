9d8a7a7cac2db570e0159f8a3ec0ff9b431343af
Franco Lanzillotta <flanzi@fi.mdp.edu.ar>
2020-08-19 13:04:06 -0300

    CustomSelect implemented with select tag

--- www/scripts/modal/CustomSelect.js
+++ www/scripts/modal/CustomSelect.js
@@ -1,54 +1,20 @@
-class CustomSelect extends HTMLElement {
-  get value() {
-    return this._value;
-  }
-
-  set value(value) {
-    if (this._value !== value) {
-      if (this._value !== undefined) {
-        const option = this._options.get(this._value);
-        option.toggleAttribute('selected', false);
-      }
-
-      this._value = value;
-
-      const option = this._options.get(value);
-      option.toggleAttribute('selected', true);
-
-      this._text.textContent = option.textContent;
-
-      this.dispatchEvent(new Event('change', { bubbles: true }));
-    }
-  }
-
+class CustomSelect extends HTMLSelectElement {
   constructor(value, options) {
     super();
 
-    this.tabIndex = 0;
-
-    this._text = document.createElement('span');
-    this.append(this._text);
-
-    const dropdown = document.createElement('div');
-    this.append(dropdown);
-
-    this._options = new Map();
+    this.setAttribute('is', 'custom-select');
 
     options.forEach((text, value) => {
-      const option = document.createElement('span');
+      const option = document.createElement('option');
       option.textContent = text;
-      dropdown.append(option);
-      this._options.set(value, option);
-      option.addEventListener('click', () => {
-        this.value = value;
-        this.blur();
-      });
+      option.value = value;
+      this.append(option);
     });
 
     this.value = value;
   }
 }
 
-customElements.define('custom-select', CustomSelect);
+customElements.define('custom-select', CustomSelect, { extends: 'select' });
 
 export default CustomSelect;
