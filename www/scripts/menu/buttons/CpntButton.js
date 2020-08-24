038998f2c7cd94cb9107f04dd217bdc5e23306b5
Franco Lanzillotta <flanzi@fi.mdp.edu.ar>
2020-08-23 23:06:19 -0300

    CpntButton open property added

--- www/scripts/menu/buttons/CpntButton.js
+++ www/scripts/menu/buttons/CpntButton.js
@@ -1,16 +1,25 @@
 import MenuButton from '../MenuButton.js';
 
 class CpntButton extends MenuButton {
+  get open() {
+    return this.active;
+  }
+
+  set open(open) {
+    this._gallery.open = open;
+    this.active = open;
+  }
+
   constructor(gallery) {
     const title = 'Componentes';
     const icon = 'images/menu/cpnt.svg';
 
     super(title, icon);
 
-    this.addEventListener('click', () => {
-      gallery.open = !gallery.open;
-      this.active = gallery.open;
-    });
+    this._gallery = gallery;
+
+    this.addEventListener('click', () =>
+      this.open = !this._gallery.open);
   }
 }
 
