9014137931c03b87e143a5275065602251da4a63
Franco Lanzillotta <flanzi@fi.mdp.edu.ar>
2020-12-05 15:16:51 -0300

    Mask interface used in SplitterInfo interface

--- src/www/scripts/ifaces/cpntInfo/SplitterInfo.ts
+++ src/www/scripts/ifaces/cpntInfo/SplitterInfo.ts
@@ -1,7 +1,8 @@
 import CpntInfo from '../CpntInfo.js';
+import Mask from '../Mask.js';
 
 interface SplitterInfo extends CpntInfo {
-  masks: Array<{ name: string, size: number }>,
+  masks: Array<Mask>,
 }
 
 export default SplitterInfo;
